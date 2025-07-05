df507e931cbd2e45e396f629df5d5f8e33cc6497
oluwafemi <oluwafemidojumo@gmail.com>
2025-07-05 14:21:56 +0100

    batch

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,10 +1,17 @@
 # fetch and process data in batches
 def stream_users_in_batches(batch_size):
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM TABLE user_data')
+    cursor.execute('SELECT * FROM user_data')
     while True:
         rows = cursor.fetchmany(batch_size)
         if not rows:
             break
         yield rows
     cursor.close()
+
+
+def batch_processing():
+    for batch in stream_users_in_batches():
+        filter_user = [user for user in batch if user['age'] > 25]
+        for user in filter_user:
+            print(f'User: {user['name']}, Age: {user['age']}')
