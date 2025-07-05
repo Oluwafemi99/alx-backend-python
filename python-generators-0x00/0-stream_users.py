01ff1dc425a1b3664ba592441a0fa3641cbed8d5
oluwafemi <oluwafemidojumo@gmail.com>
2025-07-05 12:47:10 +0100

    stream_generators

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,7 +1,7 @@
 # Stream rows one after the other
 def stream_users(connection):
     cursor = connection.cursor()
-    cursor.excute('SELECT * FROM TABLE')
+    cursor.excute('SELECT * FROM TABLE user_data')
     for row in cursor:
         yield row
     cursor.close()
