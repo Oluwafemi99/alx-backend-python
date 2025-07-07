f0c18f201852942c6dc0be723db77ec691e409cd
oluwafemi <oluwafemidojumo@gmail.com>
2025-07-07 13:18:24 +0100

    decorator_transaction

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,15 @@
+import functools
+
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            print(f'Transaction error. rolled back due to {e}')
+            raise
+    return wrapper
