55bc0b237d2293960d5fa8d25a545658055b325b
oluwafemi <oluwafemidojumo@gmail.com>
2025-07-20 18:05:18 +0100

    models

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from .models import Conversation, Message
+from .serializers import MessageSerializer, ConversationSerializer
+
 
 # Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create a new conversation with participants.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['get'])
+    def messages(self, request, pk=None):
+        """
+        Get messages for a specific conversation
+        """
+        conversation = self.get_object()
+        messages = Message.objects.filter(conversation=conversation)
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+
+class MessageViewset(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Send a message to a conversation.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
