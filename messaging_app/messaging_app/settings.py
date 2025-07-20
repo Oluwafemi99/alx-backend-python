3fae09d6d582628bb1cc9453374d2cfc0339781b
oluwafemi <oluwafemidojumo@gmail.com>
2025-07-20 18:48:51 +0100

    url

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'Oluwafemi1.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
