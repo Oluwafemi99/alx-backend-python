81096e9f4af37df0ef604f92ec9f2a07e3efa210
oluwafemi <oluwafemidojumo@gmail.com>
2025-08-02 07:13:29 +0100

    signals

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
-from .models import Message, Notification, MessageHistory
+from .models import Message, Notification, MessageHistory, User
 
 
 # signal to create notification when a new message is triggered
@@ -22,3 +22,16 @@ def log_history(instance, sender, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+
+# signal to delete all related object after User deletion
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # delete all messages where user is the sender
+    Message.objects.filter(sender=instance).delete()
+    # delete all message where user is the receiver
+    Message.objects.filter(receiver=instance).delete()
+    # delete all User notification
+    Notification.objects.filter(user=instance).delete()
+    # delete all message history
+    MessageHistory.objects.filter(edited_by=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
+
 # Create your views here.
+@require_POST
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)
+    user.delete()
+    return HttpResponse('Your accont has been succefully deleted')
